d417617da164370ed384ff66bb7123630425ebc3
chris <christian.schwind@smail.th-koeln.de>
2024-01-19 21:50:34 +0100

    Client: Implement initial post fetching and basic pagination in DiscussionSpace

--- client/src/components/DiscussionSpace.js
+++ client/src/components/DiscussionSpace.js
@@ -1,29 +1,64 @@
 import './DiscussionSpace.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import MessageCard from "./MessageCard";
 import MessageInput from "./MessageInput";
 
 function DiscussionSpace () {
-    const [messages, setMessages] = useState([{ title: 'Title', content: 'Message.', author: 'Author', timestamp: '0:00 Uhr - 01.01.24', authorPictureUrl: 'nothing working'}]);
+    const [messages, setMessages] = useState([]);
+    const [page, setPage] = useState(1);
+    const limit = 30;
 
+    useEffect(() => {
+        fetchPosts(1, 30)
+    }, [page, limit]);
+
+    async function fetchPosts (page, limit) {
+        const queryParams = new URLSearchParams({ page, limit}).toString();
+        try{
+            const response = await fetch(`http://localhost:5555/discussionSpace?${queryParams}`);
+            if (!response.ok){
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const posts = await response.json();
+            setMessages(posts);
+        } catch (error) {
+            console.error('Failed to fetch posts:', error);
+        }
+    }
+
+
+    function nextPage() {           // todo
+        setPage(page + 1);
+    }
+    function previousPage() {       // todo
+        if (page > 1) {
+            setPage(page - 1);
+        }
+    }
+
+    // post a new message
     function addMessage([message]) {
         setMessages([...messages, message])
     }
 
-    //TODO: add key for child elements
     return (
         <div className="messageBlock">
             {messages.map(message => (
                 <MessageCard
-                    key={"s"}
+                    key={message._id}
                     title={message.title}
                     content={message.content}
-                    author={message.author}
+                    author={message.author.username}
                     timestamp={message.timestamp}
                     authorPictureUrl={message.authorPictureUrl}
                 />
                 ))};
             <MessageInput/>
+            <section className="pagination">
+                <button onClick={nextPage}>Next</button>
+                <span>You're on page {page}.</span>
+                <button onClick={previousPage}>Previous</button>
+            </section>
         </div>
     );
 }
