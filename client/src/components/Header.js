7e45851c7dd6059042b383b20d060a4b5386bf9d
chris <christian.schwind@smail.th-koeln.de>
2024-03-10 14:57:23 +0100

    Client: Implement navigation toggle for enhanced mobile responsiveness in Header; add logic to close navigation with specific clicks; update CSS for improved layout and interaction across breakpoints

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,48 @@
+import {useEffect, useRef, useState} from 'react';
 import {useAuth} from "../context/AuthContext";
 import { NavLink, Link } from 'react-router-dom';
 import './Header.css';
 
-function Header () {
+
+function Header() {
     const { isLoggedIn, user, logout } = useAuth();
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const navRef = useRef();
+    const navToggleRef = useRef();
+
+    const closeNav = () => setIsNavVisible(false);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (navRef.current && navToggleRef.current && !navRef.current.contains(event.target) && !navToggleRef.current.contains(event.target)) {
+                setIsNavVisible(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener("touchstart", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener("touchstart", handleClickOutside);
+        };
+    }, [navRef, navToggleRef]);
+
 
     return (
         <header className="header">
             <div className="logo">
-                <NavLink to="/"><img src="PlaceholderLogoCollectiveSynthesis2.jpg" alt="Your Logo"/></NavLink>
+                <NavLink to="/"><img src="PlaceholderLogoCollectiveSynthesis.jpg" alt="Your Logo"/></NavLink>
             </div>
-            <nav className="navigation">
-                <NavLink to="/" className={({ isActive }) => isActive ? 'active' : undefined}>Home</NavLink>
-                <NavLink to="/section3" className={({ isActive }) => isActive ? 'active' : undefined}>Problem Space</NavLink>
-                <NavLink to="/solutions" className={({ isActive }) => isActive ? 'active' : undefined}>Solutions</NavLink>
-                <NavLink to="/discussionSpace" className={({ isActive }) => isActive ? 'active' : undefined}>Discussion Space</NavLink>
-                <NavLink to="/section5" className={({ isActive }) => isActive ? 'active' : undefined}>Considerations</NavLink>
+            <button ref={navToggleRef} className="nav-toggle" onClick={() => setIsNavVisible(!isNavVisible)}>
+                <span>☰</span>
+            </button>
+            <nav ref={navRef} className={`navigation ${isNavVisible ? 'displayed' : ''}`}>
+                <NavLink to="/" onClick={closeNav} className={({isActive}) => isActive ? 'active' : undefined}>Home</NavLink>
+                <NavLink to="/section3" onClick={closeNav} className={({isActive}) => isActive ? 'active' : undefined}>Problem Space</NavLink>
+                <NavLink to="/solutions" onClick={closeNav} className={({isActive}) => isActive ? 'active' : undefined}>Solutions</NavLink>
+                <NavLink to="/discussionSpace" onClick={closeNav} className={({isActive}) => isActive ? 'active' : undefined}>Discussion Space</NavLink>
+                <NavLink to="/section5" onClick={closeNav} className={({isActive}) => isActive ? 'active' : undefined}>Considerations</NavLink>
             </nav>
             <div className="user-section">
                 {isLoggedIn ? (
