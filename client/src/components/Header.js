cbd6787f4f1993dcf98e4ef2abed76c5e3d58bef
chris <christian.schwind@smail.th-koeln.de>
2024-03-12 12:58:25 +0100

    Client: Implement user menu toggle in Header; refine Header responsiveness and hover effects; improve visual consistency in Header across logo, navigation, and user section styling for enhanced UI aesthetics

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,24 @@
-import {useState} from 'react';
-import {useAuth} from "../context/AuthContext";
-import {NavLink, Link} from 'react-router-dom';
 import './Header.css';
+import {useState} from 'react';
+import {NavLink, Link} from 'react-router-dom';
+import {useAuth} from "../context/AuthContext";
+import {useToasts} from "../context/ToastContext";
 import useOutsideClick from "../context/useOutsideClickHook";
 
 
 function Header() {
+    const {addToast} = useToasts();
     const {isLoggedIn, user, logout} = useAuth();
     const [isNavMenuVisible, setIsNavMenuVisible] = useState(false);
+    const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
+
+    const showWIPMessage = () => addToast("Unfortunately, this section has not been built yet.", 5000)
 
     const closeNavMenu = () => setIsNavMenuVisible(false);
     const navMenuRef = useOutsideClick(closeNavMenu);
+
+    const closeUserMenu = () => setIsUserMenuVisible(false);
+    const userMenuRef = useOutsideClick(closeUserMenu);
 
     return (
         <header className="header">
@@ -18,10 +26,10 @@ function Header() {
                 <NavLink to="/"><img src="PlaceholderLogoCollectiveSynthesis.jpg" alt="Your Logo"/></NavLink>
             </div>
             <div ref={navMenuRef}>
-                <button className="nav-toggle" onClick={() => setIsNavMenuVisible(!isNavMenuVisible)}>
+                <button className="nav-menu-toggle" onClick={() => setIsNavMenuVisible(!isNavMenuVisible)}>
                     <span>☰</span>
                 </button>
-                <nav className={`navigation ${isNavMenuVisible ? 'displayed' : ''}`}>
+                <nav className={`nav-menu${isNavMenuVisible ? ' displayed' : ''}`}>
                     <NavLink to="/" onClick={closeNavMenu} className={({isActive}) => isActive ? 'active' : undefined}>Home</NavLink>
                     <NavLink to="/section3" onClick={closeNavMenu} className={({isActive}) => isActive ? 'active' : undefined}>Problem Space</NavLink>
                     <NavLink to="/solutions" onClick={closeNavMenu} className={({isActive}) => isActive ? 'active' : undefined}>Solutions</NavLink>
@@ -31,9 +39,22 @@ function Header() {
             </div>
             <div className="user-section">
                 {isLoggedIn ? (
-                    <div>
-                        <span>{user ? "Logged in as " + user.username : "Loading..."}</span>
-                        <button onClick={logout} className="logout-button">Logout</button>
+                    <div ref={userMenuRef}>
+                        <div className={`user-menu-toggle${isUserMenuVisible ? ' menu-open' : ''}`} onClick={() => setIsUserMenuVisible(!isUserMenuVisible)}>
+                            <span>{user.username}</span>
+                            {user.profilePicture ? (
+                                <img src={user.profilePicture} alt="User Profile"/>
+                            ) : (
+                                <img src={'https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg'} alt={`${user.username}'s profile picture`}></img>
+                            )}
+
+                        </div>
+                        <nav className={`user-menu${isUserMenuVisible ? ' displayed' : ''}`}>
+                            <NavLink to="#" onClick={showWIPMessage} style={{ fontWeight: 'normal' }}>User Profile</NavLink>
+                            <NavLink to="#" onClick={showWIPMessage} style={{fontWeight: 'normal'}}>Settings</NavLink>
+                            <NavLink to="#" onClick={showWIPMessage} style={{fontWeight: 'normal'}}>Other Stuff</NavLink>
+                            <button onClick={() => {closeUserMenu(); logout();}} className="logout-button">Logout</button>
+                        </nav>
                     </div>
                 ) : (
                     <Link to="/login" className="login-button">Login</Link>
