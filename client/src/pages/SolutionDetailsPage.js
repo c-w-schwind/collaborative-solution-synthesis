fc4f16ade77388164ed27810bf581796232fb4f5
chris <christian.schwind@smail.th-koeln.de>
2024-04-01 12:03:02 +0200

    Client: Implement SolutionDetailsPage component with fetch logic & CSS file

---
+++ client/src/pages/SolutionDetailsPage.js
@@ -0,0 +1,39 @@
+import './SolutionDetailsPage.css';
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+
+function SolutionDetailsPage() {
+    const {solutionNumber} = useParams();
+    const [solution, setSolution] = useState(null);
+
+    useEffect( () => {
+        const fetchSolution = async () => {
+            try{
+                const response = await fetch(`http://localhost:5555/solutions/${solutionNumber}`)
+                if (!response.ok){
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                setSolution(data.solution);
+            } catch (err) {
+                console.error('Failed to fetch solution:', err);
+            }
+        }
+
+        fetchSolution();
+    }, [solutionNumber]);
+
+
+    return (
+        solution ? (
+            <>
+                <div className="solution-details-container"></div>
+            </>
+        ) : (
+            <p>Loading solution details...</p>
+        )
+    );
+}
+
+export default SolutionDetailsPage;
