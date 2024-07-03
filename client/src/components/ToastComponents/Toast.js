c3ca1b6ed3b1a9975610e7f87edc1cab2efae6c5
chris <christian.schwind@smail.th-koeln.de>
2024-07-03 10:12:50 +0200

    Client: Refactor Toast component for improved structure and flexibility & update Toast CSS for better layout and alignment

--- client/src/components/ToastComponents/Toast.js
+++ client/src/components/ToastComponents/Toast.js
@@ -4,11 +4,11 @@ function Toast({ message, onClose, timeout }) {
     const fadeOutStart = timeout - 800;
 
     return (
-        <div className="toast" style={{
-            animation: `slideIn 0.5s, fadeOut 0.8s ${fadeOutStart}ms forwards`
-        }}>
-            {message}
-            <button onClick={onClose} className="toast-close-button">X</button>
+        <div className="toast" style={{animation: `slideIn 0.5s, fadeOut 0.8s ${fadeOutStart}ms forwards`}}>
+            <div className="toast-content">
+                <span className="toast-message">{message}</span>
+                <button onClick={onClose} className="toast-close-button">X</button>
+            </div>
         </div>
     );
 }
