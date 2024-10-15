8360b0c703531e5348fb65347e32ee14989ddbb7
chris <christian.schwind@smail.th-koeln.de>
2024-10-15 10:19:08 +0200

    refactor: optimize LayoutContext with useMemo; remove scrollbarWidth from context; update CSS for new scrollbar handling

--- client/src/context/LayoutContext.js
+++ client/src/context/LayoutContext.js
@@ -1,27 +1,28 @@
-import {createContext, useState, useEffect, useContext} from "react";
+import {createContext, useState, useEffect, useContext, useMemo} from "react";
 import {getScrollbarWidth} from "../utils/utils";
 
 export const LayoutContext = createContext();
 
 export const LayoutProvider = ({children}) => {
     const [isOverlayActive, setIsOverlayActive] = useState(false);
-    const [scrollbarWidth, setScrollbarWidth] = useState(0);
     const [elementOverlayColor, setElementOverlayColor] = useState("rgba(0, 0, 0, 0.5)");
 
     useEffect(() => {
         if (isOverlayActive) {
-            setScrollbarWidth(getScrollbarWidth());
             document.body.style.overflow = "hidden";
             document.body.style.paddingRight = `${getScrollbarWidth()}px`;
         } else {
             document.body.style.overflow = "";
             document.body.style.paddingRight = "";
-            setScrollbarWidth(0);
         }
     }, [isOverlayActive]);
 
+    const value = useMemo(() => ({
+        isOverlayActive, setIsOverlayActive, elementOverlayColor, setElementOverlayColor
+    }),[isOverlayActive, elementOverlayColor]);
+
     return (
-        <LayoutContext.Provider value={{setIsOverlayActive, scrollbarWidth, elementOverlayColor, setElementOverlayColor}}>
+        <LayoutContext.Provider value={value}>
             {children}
         </LayoutContext.Provider>
     );
